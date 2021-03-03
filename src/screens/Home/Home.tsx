7a7b9bd0f302a41d5b5ef3f7b6f6f6e3a9c06031
DomBart <domantas.bartusevicius@gmail.com>
2021-03-03 12:32:29 +0200

    Project setup

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.scss';
+import ConverterCard from '../../components/ConverterCard/ConverterCard'
+
+const Home = () => {
+    return (
+        <div className='home__container'>
+           <ConverterCard />
+        </div>
+    )
+}
+
+export default Home
